8815fe7caac15d2b6477d197e5b855c42af47227
sgraziola <simo.fdl@gmail.com>
2023-01-20 15:11:42 +0100

    Add new API route & show fun (Milestone 1)

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -15,4 +15,20 @@ class ProjectController extends Controller
             'results' => Project::with(['type', 'technologies'])->orderByDesc('id')->paginate(8),
         ]);
     }
+
+    public function show($slug)
+    {
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'results' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => null
+            ]);
+        }
+    }
 }
